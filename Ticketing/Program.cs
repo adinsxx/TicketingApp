f4310c33146b867f665ddc67c8e951debdd78ce0
Connor <c.mccurrie@gmail.com>
2020-09-01 18:50:10 -0500

    Initial Commit

--- Ticketing/Program.cs
+++ Ticketing/Program.cs
@@ -43,7 +43,8 @@ namespace TicketingApp
                 }
                 else if (choice == "2")
                 {
-                                        StreamWriter sw = new StreamWriter(file);
+                    StreamWriter sw = new StreamWriter(file);
+                    int ticketID = 0;
                     for (int i = 0; i < 7; i++)
                     {
                         // ask a question
@@ -51,8 +52,9 @@ namespace TicketingApp
                         // input the response
                         string resp = Console.ReadLine().ToUpper();
                         // if the response is anything other than "Y", stop asking
-                        if (resp != "Y") { break; }
+                        if (resp != "Y") { break; }                      
                         // short summary of issue
+                        ticketID++;
                         Console.WriteLine("Please enter the summary of the issue:");
                         // save the summary
                         string summary = Console.ReadLine();
@@ -61,13 +63,19 @@ namespace TicketingApp
                         // save the course grade
                         string status = Console.ReadLine();
                         // enter pritority of ticket
-                        Console.WriteLine("What is the priority of this ticket? (Low/Med/High)")
+                        Console.WriteLine("What is the priority of this ticket? (Low/Med/High)");
                         // save priority level
                         string priority = Console.ReadLine();
                         // prompt name of submitter
-                        Console.WriteLine("Who is submitting this ticket?")
+                        Console.WriteLine("Who is submitting this ticket?");
                         string submitter = Console.ReadLine();
-                        sw.WriteLine("{0}|{1}|{2}|{3}", ticketID, summary, status, priority);
+                        // name of assigned analyst
+                        Console.WriteLine("Who is assigned this ticket?");
+                        string assigned = Console.ReadLine();
+                        // name of person watching this ticket
+                        Console.WriteLine("Who is watching this ticket?");
+                        string watching = Console.ReadLine();
+                        sw.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}", ticketID, summary, status, priority, submitter, assigned, watching);
                     }
                     sw.Close();
                 }
