e1e8a9c204645584cdb9da466104127ad4a597ea
Connor <c.mccurrie@gmail.com>
2020-09-29 20:39:21 -0500

    Separating out code to specific classes

---
+++ Ticketing/ReadData.cs
@@ -0,0 +1,23 @@
+// if (choice == "1")
+// {
+// // read data from file
+//     if (File.Exists(file))
+//     {
+//         //ticketID accumulator
+//         // read data from file
+//         StreamReader sr = new StreamReader(file);
+//         while (!sr.EndOfStream)
+//         {
+//             string line = sr.ReadLine();
+//             // convert string to array
+//             string[] arr = line.Split('|');
+//             // display array data
+//             Console.WriteLine("TicketID: {0}, Summary: {1}, Status: {2}, Priority: {3}, Submitter: {4}, Assigned: {5}, Watching: {6}", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);
+//         }
+//         sr.Close();
+//     }
+//     else
+//     {
+//         Console.WriteLine("File does not exist");
+//     }
+// }
---
+++ Ticketing/WriteData.cs
@@ -0,0 +1,38 @@
+// else if (choice == "2")
+//                 {
+//                     StreamWriter sw = new StreamWriter(file);
+//                     int ticketID = 0;
+//                     for (int i = 0; i < 7; i++)
+//                     {
+//                         // ask a question
+//                         Console.WriteLine("Reporting an issue? (Y/N)");
+//                         // input the response
+//                         string resp = Console.ReadLine().ToUpper();
+//                         // if the response is anything other than "Y", stop asking
+//                         if (resp != "Y") { break; }                      
+//                         // short summary of issue
+//                         ticketID++;
+//                         Console.WriteLine("Please enter the summary of the issue:");
+//                         // save the summary
+//                         string summary = Console.ReadLine();
+//                         // prompt for ticket status
+//                         Console.WriteLine("What is the ticket status? (Open/Resolved)");
+//                         // save the course grade
+//                         string status = Console.ReadLine();
+//                         // enter pritority of ticket
+//                         Console.WriteLine("What is the priority of this ticket? (Low/Med/High)");
+//                         // save priority level
+//                         string priority = Console.ReadLine();
+//                         // prompt name of submitter
+//                         Console.WriteLine("Who is submitting this ticket?");
+//                         string submitter = Console.ReadLine();
+//                         // name of assigned analyst
+//                         Console.WriteLine("Who is assigned this ticket?");
+//                         string assigned = Console.ReadLine();
+//                         // name of person watching this ticket
+//                         Console.WriteLine("Who is watching this ticket?");
+//                         string watching = Console.ReadLine();
+//                         sw.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}", ticketID, summary, status, priority, submitter, assigned, watching);
+//                     }
+//                     sw.Close();
+//                 }
