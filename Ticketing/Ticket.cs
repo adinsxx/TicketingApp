2779698ed66215ff95aaca0ebb0d3f3325ed29ac
Connor <c.mccurrie@gmail.com>
2020-10-04 19:50:57 -0500

    Major commit for pull and finishing up abstract classes

--- Ticketing/Ticket.cs
+++ Ticketing/Ticket.cs
@@ -10,13 +10,21 @@ namespace TicketingApp
         public string submitter {get; set;}
         public string assigned {get; set;}
         public List<string> watching {get; set;}
-        public string severity {get; set;}
 
-
-        public virtual string Display(){
-            return $"ID: {ticketId}\nSummary: {summary}\nPriority: {priority}\nSubmitter: {submitter}\nAssigned: {assigned}\nWatching: {string.Join(", ", watching)}\n";
+        public Ticket(){
+            watching = new List<string>();
         }
 
+        public virtual string Display(){
+        return $"ID: {ticketId}\nSummary: {summary}\nPriority: {priority}\nSubmitter: {submitter}\nAssigned: {assigned}\nWatching: {string.Join(", ", watching)}\n";
+        }
+    }
+    public class Incidents : Ticket
+    {
+        public string severity {get; set;}
+        public override string Display(){
+        return $"ID: {ticketId}\nSummary: {summary}\nPriority: {priority}\nSubmitter: {submitter}\nAssigned: {assigned}\nWatching: {string.Join(", ", watching)}\nSeverity: {severity}";  
+        }
     }
 
 
